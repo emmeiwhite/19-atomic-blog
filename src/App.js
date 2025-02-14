8d7b0af053e6bc2c935540a79a9363de8cda9715
emmeiwhite <emmeiwhite@gmail.com>
2025-02-14 16:02:59 +0530

    context-api-1.1 Array.from() JS stuff with useEffect to revisit and recall

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import { useEffect, useState } from "react";
-import { faker } from "@faker-js/faker";
+import { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker'
 
 function createRandomPost() {
   return {
     title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
+    body: faker.hacker.phrase()
+  }
 }
 
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-  const [searchQuery, setSearchQuery] = useState("");
-  const [isFakeDark, setIsFakeDark] = useState(false);
+  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()))
+  const [searchQuery, setSearchQuery] = useState('')
+  const [isFakeDark, setIsFakeDark] = useState(false)
 
+  function sayHello() {
+    console.log('hello')
+  }
+
+  useEffect(() => {
+    Array.from({ length: 10 }, () => sayHello()) // the second argument is a mapper function invoked for each element of the Array.
+  }, [])
   // Derived state. These are the posts that will actually be displayed
   const searchedPosts =
     searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
+      ? posts.filter(post =>
+          `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase())
         )
-      : posts;
+      : posts
 
   function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
+    setPosts(posts => [post, ...posts])
   }
 
   function handleClearPosts() {
-    setPosts([]);
+    setPosts([])
   }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
-      document.documentElement.classList.toggle("fake-dark-mode");
+      document.documentElement.classList.toggle('fake-dark-mode')
     },
     [isFakeDark]
-  );
+  )
 
   return (
     <section>
       <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
+        onClick={() => setIsFakeDark(isFakeDark => !isFakeDark)}
+        className="btn-fake-dark-mode">
+        {isFakeDark ? '☀️' : '🌙'}
       </button>
 
       <Header
@@ -56,11 +58,14 @@ function App() {
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
-      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Main
+        posts={searchedPosts}
+        onAddPost={handleAddPost}
+      />
       <Archive onAddPost={handleAddPost} />
       <Footer />
     </section>
-  );
+  )
 }
 
 function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
@@ -78,21 +83,21 @@ function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
         <button onClick={onClearPosts}>Clear posts</button>
       </div>
     </header>
-  );
+  )
 }
 
 function SearchPosts({ searchQuery, setSearchQuery }) {
   return (
     <input
       value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
+      onChange={e => setSearchQuery(e.target.value)}
       placeholder="Search posts..."
     />
-  );
+  )
 }
 
 function Results({ posts }) {
-  return <p>🚀 {posts.length} atomic posts found</p>;
+  return <p>🚀 {posts.length} atomic posts found</p>
 }
 
 function Main({ posts, onAddPost }) {
@@ -101,7 +106,7 @@ function Main({ posts, onAddPost }) {
       <FormAddPost onAddPost={onAddPost} />
       <Posts posts={posts} />
     </main>
-  );
+  )
 }
 
 function Posts({ posts }) {
@@ -109,36 +114,36 @@ function Posts({ posts }) {
     <section>
       <List posts={posts} />
     </section>
-  );
+  )
 }
 
 function FormAddPost({ onAddPost }) {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
 
   const handleSubmit = function (e) {
-    e.preventDefault();
-    if (!body || !title) return;
-    onAddPost({ title, body });
-    setTitle("");
-    setBody("");
-  };
+    e.preventDefault()
+    if (!body || !title) return
+    onAddPost({ title, body })
+    setTitle('')
+    setBody('')
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={e => setTitle(e.target.value)}
         placeholder="Post title"
       />
       <textarea
         value={body}
-        onChange={(e) => setBody(e.target.value)}
+        onChange={e => setBody(e.target.value)}
         placeholder="Post body"
       />
       <button>Add post</button>
     </form>
-  );
+  )
 }
 
 function List({ posts }) {
@@ -151,7 +156,7 @@ function List({ posts }) {
         </li>
       ))}
     </ul>
-  );
+  )
 }
 
 function Archive({ onAddPost }) {
@@ -159,15 +164,15 @@ function Archive({ onAddPost }) {
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
-  );
+  )
 
-  const [showArchive, setShowArchive] = useState(false);
+  const [showArchive, setShowArchive] = useState(false)
 
   return (
     <aside>
       <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      <button onClick={() => setShowArchive(s => !s)}>
+        {showArchive ? 'Hide archive posts' : 'Show archive posts'}
       </button>
 
       {showArchive && (
@@ -183,11 +188,11 @@ function Archive({ onAddPost }) {
         </ul>
       )}
     </aside>
-  );
+  )
 }
 
 function Footer() {
-  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+  return <footer>&copy; by The Atomic Blog ✌️</footer>
 }
 
-export default App;
+export default App
