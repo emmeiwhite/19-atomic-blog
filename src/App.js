7ed886424dd0fd3ccb7a8d9bf628c61452073eca
emmeiwhite <emmeiwhite@gmail.com>
2025-02-14 20:16:01 +0530

    context-api-1.3 Prop-Drilling issue resolved with Context API

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       : posts
 
   function handleAddPost(post) {
-    setPosts(posts => [post, ...posts])
+    setPosts(posts => [...posts, post])
   }
 
   function handleClearPosts() {
@@ -101,16 +101,17 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>
 }
 
-function Main({ posts, onAddPost }) {
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts />
     </main>
   )
 }
 
-function Posts({ posts }) {
+function Posts() {
+  const { posts } = useContext(PostContext)
   return (
     <section>
       <List posts={posts} />
@@ -118,9 +119,11 @@ function Posts({ posts }) {
   )
 }
 
-function FormAddPost({ onAddPost }) {
+function FormAddPost() {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
+
+  const { onAddPost } = useContext(PostContext)
 
   const handleSubmit = function (e) {
     e.preventDefault()
@@ -147,7 +150,8 @@ function FormAddPost({ onAddPost }) {
   )
 }
 
-function List({ posts }) {
+function List() {
+  const { posts } = useContext(PostContext)
   return (
     <ul>
       {posts.map((post, i) => (
@@ -164,7 +168,7 @@ function Archive({ onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
+    Array.from({ length: 100 }, () => createRandomPost())
   )
 
   const [showArchive, setShowArchive] = useState(false)
@@ -192,6 +196,7 @@ function Archive({ onAddPost }) {
   )
 }
 
+// Just a normal Presentational Component
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>
 }
